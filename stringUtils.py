68d285ae50593a28659985b8f16cba38ac087c34
JamesRMR <james.richmond@rmrbenefits.com>
2024-03-27 19:08:04 -0600

    added string utils class

---
+++ stringUtils.py
@@ -0,0 +1,9 @@
+class StringUtils:
+    def concatenate(self, str1, str2):
+        return str1 + str2
+    
+    def extract_substring(self, string, start, end):
+        return string[start:end]
+    
+    def capitalize(self, string):
+        return string.capitalize()
